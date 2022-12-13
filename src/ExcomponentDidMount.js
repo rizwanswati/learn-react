810b792ad2eaab6494172054a49bed82f76f14f7
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-13 17:47:42 +0500

    LifeCycleMethod: - componentDidUpdate - shouldComponentUpdate

--- src/ExcomponentDidMount.js
+++ src/ExcomponentDidMount.js
@@ -12,7 +12,8 @@ class ExcomponentDidMount extends react.Component{
         super();
         console.warn('Constructor')
         this.state = {
-            name:'Rizwan'
+            name:'Rizwan',
+            count:0
         }
     }
 
@@ -20,12 +21,37 @@ class ExcomponentDidMount extends react.Component{
         console.warn('componentDidMount')
     }
 
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.warn('shouldComponentUpdate');
+        if (this.state.count>5 && this.state.count<10){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.warn(prevState,this.state.count)
+        /*
+        * Lets assume you are getting state from an API call and wanna check if previous state and new state are different
+        * */
+
+        if (this.state.count === prevState){
+            console.log('Both are Same')
+        }else{
+            console.error('previous and new states are not same')
+        }
+    }
+
+
     render() {
         console.warn('render')
         return (
             <div>
                 <h2>LifeCycleMethodEx::componentDidMount {this.state.name}</h2>
+                <h2>count:: {this.state.count}</h2>
                 <button onClick={()=>this.setState({name:'Ullah'})}>Update</button>
+                <button onClick={()=>this.setState({count:this.state.count+1})}>Incriment</button>
             </div>
         )
     }
