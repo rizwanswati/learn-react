e28df5779323672ff1c0d074aaf5b8c2561581f6
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-24 11:20:15 +0500

    Routers Setup

--- src/Engine.js
+++
@@ -1,7 +0,0 @@
-import React,{forwardRef} from "react";
-function Engine(props, ref) {
-    return(
-      <input type={"text"} ref={ref}/>
-    );
-}
-export default forwardRef(Engine);
--- src/Participants.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from "react";
-
-export default class Participants extends Component{
-    render() {
-        return (
-            <h3>This is a class based component</h3>
-        );
-    }
-}
