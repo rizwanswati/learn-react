c1b86f25b787df3565e7eb79427e577da6fae4a9
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-24 11:58:27 +0500

    Links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
 import Home from "./component/Home";
 import About from "./component/About";
+import NaBar from "./component/NaBar";
 
 function App() {
     return (
         <div className="App">
             <h1>Learning react</h1>
             <BrowserRouter>
+                <NaBar/>
                 <Routes>
                     <Route path={"/"} element={<About/>}/>
                     <Route path="/home" element={<Home/>}/>
---
+++ src/component/NaBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {ListGroup} from "react-bootstrap";
+
+function NaBar() {
+    return (
+        <di>
+            <ListGroup as="ul">
+                <ListGroup.Item as="li"><Link to="/Home">Home</Link></ListGroup.Item>
+                <ListGroup.Item as="li"><Link to="/">About</Link></ListGroup.Item>
+            </ListGroup>
+        </di>
+    )
+}
+
+export default NaBar
