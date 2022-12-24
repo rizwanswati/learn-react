ddca34f2673def82f4906bb889fa6a480d492479
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-24 14:39:49 +0500

    Navigating Programmatically

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {useNavigate} from "react-router-dom"
 
 function Home() {
+    const about = useNavigate();
+    const gotToAbout = (url) => {
+        about(url)
+    }
     return <>
         <h2>Home Component</h2>
+        <button onClick={() => gotToAbout("/")}>go to about page</button>
     </>
 }
 
