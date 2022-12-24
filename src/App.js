d1dc703abae807cb942ac5a8f6b9d16397793d4b
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-24 11:33:51 +0500

    Basic routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./component/Home";
+import About from "./component/About";
+
 function App() {
     return (
-        <div className="App"><h1>Learning react</h1>
+        <div className="App">
+            <h1>Learning react</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/"} element={<About/>}/>
+                    <Route path="/home" element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
