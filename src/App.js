4cc8dc5efbf5ca5d1b0db2f03de9182cbd3eda8e
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-27 13:14:14 +0500

    previous-states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import PreviousState from "./component/PreviousState";
 
 function App() {
 
     return (
         <div className="App">
-            <h1>React APIs</h1>
+            <h1>React Previous State</h1>
+            <PreviousState/>
         </div>
     );
 }
