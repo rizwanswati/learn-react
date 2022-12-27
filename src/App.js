b75e6744c0e50e56c1fa1f6db286629e8b839746
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-27 16:00:04 +0500

    Redux basic setup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
-import {ConsumerContext} from "./component/ConsumerContext";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
-import ColorUpdateButton from "./component/ColorUpdateButton";
 
-class App extends React.Component {
-
-    constructor() {
-        super()
-
-        this.updateColor = (col) => {
-            this.setState({
-                color: col
-            })
-        }
-
-        this.state = {
-            color: "green",
-            updateColor: this.updateColor
-        }
-    }
-
-    render() {
-        return (
-            <ConsumerContext.Provider value={this.state}>
-                <Header/>
-                <Footer/>
-                <ColorUpdateButton/>
-            </ConsumerContext.Provider>
-        );
-    }
+function App() {
+    return (
+        <div className="App">
+            <h1>React Redux</h1>
+        </div>
+    );
 }
 
 export default App;
