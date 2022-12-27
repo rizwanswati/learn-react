32fb019157610a8092762df55557b239eab0a89d
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2022-12-27 13:47:50 +0500

    previous-props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import PreviousState from "./component/PreviousState";
+import {useState} from "react";
+import {Button} from "react-bootstrap";
+import PreviousProps from "./component/PreviousProps";
 
 function App() {
+    const [count, setCount] = useState(1);
+
+    function update() {
+        setCount((PreviousValue) => {
+            return Math.floor(Math.random() * 10)
+        })
+    }
 
     return (
         <div className="App">
-            <h1>React Previous State</h1>
+            <h1>React Previous State/Props</h1>
             <PreviousState/>
+            <PreviousProps count={count}/>
+            <Button variant={"outline-primary"} onClick={() => update()}>Previous Val of Count</Button>
         </div>
     );
 }
